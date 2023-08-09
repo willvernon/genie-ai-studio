7543dee74aaf3341ffb70d65dac6322049bb4e2d
William Vernon <wmvernon@proton.me>
2023-08-09 15:56:41 -0400

    feat - Sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import { Montserrat } from 'next/font/google'
+
+import logo from '@/public/Logo.png'
+import { cn } from '@/lib/utils'
+
+const montserrat = Montserrat({
+	weight: '600',
+	subsets: ['latin'],
+})
+
+const Sidebar = () => {
+	return (
+		<div className="space-y-4 py-4 flex flex-col h-full bg-slate-800 text-white">
+			<div className="px-3 py-2 flex-1">
+				<Link href="/dashboard" className="flex items-center pl-3 mb-14">
+					<div className="relative w-8 h-10 mr-4">
+						<Image fill alt="Logo" src={logo} />
+					</div>
+					<h1 className={cn('text-2xl font-bold ', montserrat.className )}>
+						AI Genie
+					</h1>
+				</Link>
+			</div>
+		</div>
+	)
+}
+
+export default Sidebar
